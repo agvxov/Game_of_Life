416d70fbeddad55908e7c4a539442c0c4fa4c3c4
anon <anon@anon.anon>
2022-12-14 17:06:47 +0100

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Game_of_Life
 
+Proof of concept Conway's Game of Life.
+
+# Todo
++ load() doesnt work
++ save() can segfault somehow; dunno how to recreate the bug tho
+
+# Notes
++ resizing the window will lead to either stripping balls or (more likely) a crash
++ if one loads a fauna that was saved under different screen size
+	everything is gonna be offset; however i hardly care
++ an insanely sized screen will cause int overflows and abnormal behaviour
--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,6 @@ bool running = false;
 unsigned int std_height = 0;
 unsigned int std_width = 0;
 
-/*--------------------------------------------Todos------------------------------------------------\\
-|	>load() doesnt work
-|	>save() can segfault somehow; dunno how to recreate the bug tho
-\\-------------------------------------------------------------------------------------------------*/
-
-/*--------------------------------------------Notes------------------------------------------------\\
-|	>resizing the window will lead to either stripping balls or (more likely) a crash
-|	>if one loads a fauna that was saved under different screen size
-|     everything is gonna be offset; however i hardly care
-|	>an insanely sized screen will cause int overflows and abnormal behaviour
-\\-------------------------------------------------------------------------------------------------*/
 
 signed main(){
 	//init
